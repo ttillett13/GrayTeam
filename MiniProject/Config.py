9c35bf2ff87b82f9e35c24f97d82ad4e3c2bc226
Joshua Comish <jomish2323@gmail.com>
2017-10-06 04:12:54 -0500

    cleaning up

--- MiniProject/Config.py
+++ MiniProject/Config.py
@@ -4,7 +4,7 @@ from google.appengine.api import app_identity
 
 # BUCKET_NAME = os.environ.get('BUCKET_NAME',
 #                                app_identity.get_default_gcs_bucket_name())
-BUCKET_NAME = app_identity.get_default_gcs_bucket_name()
+BUCKET_NAME = "staging.vibrant-mind-177623.appspot.com"
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
