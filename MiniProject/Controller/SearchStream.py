1c92754375a5861669c4fc48104b9048cb16557d
ttillett13 <ttillett@utexas.edu>
2017-10-06 23:24:51 -0500

    changed search code to get image from library if cover image doesn't already exist

--- MiniProject/Controller/SearchStream.py
+++ MiniProject/Controller/SearchStream.py
@@ -45,21 +45,26 @@ class SearchStream(webapp2.RequestHandler):
                 streams_found = index.search(query)
                 num_found = len(streams_found.results)
 
-                for stream in streams_found:
+                for stream in streams_found.results:
                     if not stream.field('cover_image').value:
-                        key_str = stream.doc_id
-                        key = ndb.Key(urlsafe=key_str)
-                        stream_obj = key.get
-                        if stream_obj.pictures:
-                            cover_image = images.get_serving_url(stream_obj.pictures[0].get().image, secure_url=False)
-                            d = search.Document(
-                                doc_id=stream.doc_id,
-                                fields=[search.TextField(name="stream_name", value=stream_obj.stream_name),
+                        #key_str = stream.doc_id.value
+                        #key = ndb.Key(urlsafe=key_str)
+                        #stream_obj = key.get
+                        #current_user = User.query(User.username == auth[0]._User__email).get()
+
+                        stream_obj = Stream.query(Stream.name == stream.field('stream_name').value).get()
+                        if stream_obj:
+                            if stream_obj.pictures:
+                                cover_image = images.get_serving_url(stream_obj.pictures[0].get().image,
+                                                                     secure_url=False)
+                                d = search.Document(
+                                    doc_id=stream.doc_id,
+                                    fields=[search.TextField(name="stream_name", value=stream_obj.stream_name),
                                         search.TextField(name="cover_image", value=cover_image),
                                         search.TextField(name="url", value=stream_obj.url),
                                         search.TextField(name="tag", value=stream.field('tag').value)],
-                                language="en")
-                            search.Index(name=INDEX_NAME).put(d)
+                                    language="en")
+                                search.Index(name=INDEX_NAME).put(d)
 
                 streams_found = index.search(query)
 
